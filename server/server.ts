4ed06599370c55faab09a478e451fc0c5618e993
roney ruiz <roneyruizrojas@gmail.com>
2025-02-20 20:25:31 -0600

    feat(server): integrate apollo with express

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,27 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import http from "http";
+import express from "express";
+import { json } from "body-parser";
+
 import { BookGraphQL } from "./src/Graphql/BookGraphQL";
 import { BookResolver } from "./src/Resolvers/BookResolver";
 
-const server = new ApolloServer({
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+const app = express();
+const server = http.createServer(app);
+
+const apollo = new ApolloServer({
   typeDefs: [BookGraphQL],
   resolvers: [BookResolver],
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer: server })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+await apollo.start();
 
-console.log(`🚀 Server ready at: ${url}`);
+app.use(json());
+app.use("/graphql", expressMiddleware(apollo));
+
+await new Promise<void>((resolve) => server.listen({ port: 3000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:3000/graphql`);
