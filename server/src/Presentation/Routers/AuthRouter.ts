9135a52b66152cfe2a89aefc0315190bc44f47dd
roney ruiz <roneyruizrojas@gmail.com>
2025-02-23 13:53:57 -0600

    feat(server): implement dependency injection for auth controller

--- server/src/Presentation/Routers/AuthRouter.ts
+++ server/src/Presentation/Routers/AuthRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import { Context } from "@Database/Context";
+
 import { AuthController } from "@Controllers/AuthController";
+import { UserRepository } from "@Repositories/UserRepository";
+
 import { SimpleAuthUseCase } from "@UseCases/Auth/SimpleAuth/SimpleAuthUseCase";
 
-const simpleAuthUseCase = new SimpleAuthUseCase();
+const repository = new UserRepository(Context);
+
+const simpleAuthUseCase = new SimpleAuthUseCase(repository);
 
 const controller = new AuthController(
   simpleAuthUseCase,
