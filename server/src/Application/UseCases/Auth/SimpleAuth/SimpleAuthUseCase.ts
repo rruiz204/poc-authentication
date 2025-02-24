ac42708161575fc15f6dc227899bb12d19de1884
roney ruiz <roneyruizrojas@gmail.com>
2025-02-23 21:08:23 -0600

    feat(server): build simple auth use case

--- server/src/Application/UseCases/Auth/SimpleAuth/SimpleAuthUseCase.ts
+++ server/src/Application/UseCases/Auth/SimpleAuth/SimpleAuthUseCase.ts
@@ -1,6 +1,8 @@
 import type { AuthDTO } from "../DTOs/AuthDTO";
 import type { UseCase } from "@UseCases/UseCase";
 import { SimpleAuthSchema } from "./SimpleAuthSchema";
+import { HasherService } from "@Services/HasherService";
+import { JwTokensService } from "@Services/JwTokensService";
 import type { SimpleAuthCommand } from "./SimpleAuthCommand";
 import type { UserRepository } from "@Repositories/UserRepository";
 
@@ -13,9 +15,10 @@ export class SimpleAuthUseCase implements UseCase<SimpleAuthCommand, AuthDTO> {
     const existing = await this.repository.find({ email: command.email });
     if (!existing) throw new Error("User not found");
 
-    return Promise.resolve({
-      type: "Bearer",
-      token: "example-token-123",
-    });
+    const verified = await HasherService.verify(command.password, existing.password);
+    if (!verified) throw new Error("Invalid password");
+
+    const token = await JwTokensService.sign({ id: existing.id });
+    return { type: "Bearer", token };
   };
 };
