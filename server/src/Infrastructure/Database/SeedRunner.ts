b61e71a3d513febe848cc0a66a7606ba0ca48adb
roney ruiz <roneyruizrojas@gmail.com>
2025-02-23 20:30:58 -0600

    feat(server): build seeding for user entity

---
+++ server/src/Infrastructure/Database/SeedRunner.ts
@@ -0,0 +1,28 @@
+import { Context } from "./Context";
+import type { Seeder } from "./Seeders/Seeder";
+
+import { UserSeeder } from "./Seeders/UserSeeder";
+import { UserRepository } from "@Repositories/UserRepository";
+
+const userRepository = new UserRepository(Context);
+const userSeeder = new UserSeeder(userRepository);
+
+const SeedRunner = async (seeders: Seeder[]) => {
+  for (const seeder of seeders) {
+    const name = seeder.getName();
+
+    const executed = await Context.seed.findFirst({
+      where: { name: { equals: name } }
+    });
+
+    if (executed) return;
+
+    await seeder.seed();
+    console.log(`Seeder executed ${name}`);
+    await Context.seed.create({ data: { name } });
+  };
+};
+
+await SeedRunner([
+  userSeeder,
+]);
---
+++ server/src/Infrastructure/Database/Seeders/UserSeeder.ts
@@ -0,0 +1,23 @@
+import type { Seeder } from "./Seeder";
+import { UserFactory } from "@Database/Factories/UserFactory";
+import type { UserRepository } from "@Repositories/UserRepository";
+
+export class UserSeeder implements Seeder {
+  private name: string = "user-seeder";
+
+  constructor(private repository: UserRepository) {};
+
+  public getName(): string {
+    return this.name;
+  };
+
+  public async seed(): Promise<void> {
+    const admin = await UserFactory.build({
+      id: 1, name: "admin",
+      email: "admin@admin.com",
+      password: "12345678"
+    });
+
+    await this.repository.create({ ...admin });
+  };
+};
