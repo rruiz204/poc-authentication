76537faaa666f2e22dffa8ae22c3471adbe9fad1
roney ruiz <roneyruizrojas@gmail.com>
2025-02-25 07:12:32 -0600

    feat(server): build user facade

---
+++ server/src/Graphql/Facades/UserFacade.ts
@@ -0,0 +1,13 @@
+import type { GraphQLContext } from "@Graphql/GraphQLContext";
+import type { UserRepository } from "@Repositories/UserRepository";
+import type { ListUsersQuery } from "@UseCases/User/ListUsers/ListUsersQuery";
+import { ListUsersUseCase } from "@UseCases/User/ListUsers/ListUsersUseCase";
+
+export class UserFacade {
+  constructor(private repository: UserRepository) {};
+
+  public async listUsers(parent: unknown, args: ListUsersQuery, context: GraphQLContext) {
+    const useCase = new ListUsersUseCase(this.repository);
+    return useCase.use(args);
+  };
+};
