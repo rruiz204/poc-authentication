3f94228b90a08fae720819d4f1cc85e4fded3631
roney ruiz <roneyruizrojas@gmail.com>
2025-02-25 07:13:41 -0600

    refactor(server): improve user resolver

--- server/src/Graphql/Resolvers/UserResolver.ts
+++ server/src/Graphql/Resolvers/UserResolver.ts
@@ -1,18 +1,12 @@
 import { Context } from "@Database/Context";
-import type { GraphQLContext } from "@Graphql/GraphQLContext";
+import { UserFacade } from "@Graphql/Facades/UserFacade";
 import { UserRepository } from "@Repositories/UserRepository";
-import type { ListUsersQuery } from "@UseCases/User/ListUsers/ListUsersQuery";
-import { ListUsersUseCase } from "@UseCases/User/ListUsers/ListUsersUseCase";
-import type { GraphQLResolveInfo } from "graphql";
 
 const repository = new UserRepository(Context);
-
-const listUsersUseCase = new ListUsersUseCase(repository);
+const facade = new UserFacade(repository);
 
 export const UserResolver = {
   Query: {
-    listUsers: async (parent: unknown, args: ListUsersQuery, contextValue: GraphQLContext, info: GraphQLResolveInfo) => {
-      return await listUsersUseCase.use({ ...args });
-    },
+    listUsers: facade.listUsers.bind(facade),
   },
 };
