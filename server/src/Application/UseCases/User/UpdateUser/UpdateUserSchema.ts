85c3372e7a2a96a315129ebc0893dbad7ab5782e
roney ruiz <roneyruizrojas@gmail.com>
2025-02-28 17:45:49 -0600

    refactor(server): move update user dto

--- server/src/Application/UseCases/User/UpdateUser/UpdateUserSchema.ts
+++ server/src/Application/UseCases/User/UpdateUser/UpdateUserSchema.ts
@@ -2,7 +2,6 @@ import * as yup from "yup";
 
 export const UpdateUserSchema = yup.object({
   name: yup.string(),
-  email: yup.string()
-    .email(),
+  email: yup.string().email(),
   active: yup.boolean(),
 });
