89bf84fb63c7662a033fa8a8058789ca969733de
roney ruiz <roneyruizrojas@gmail.com>
2025-02-28 17:12:34 -0600

    format(server): simple auth use case

--- server/src/Application/UseCases/Auth/SimpleAuth/SimpleAuthSchema.ts
+++ server/src/Application/UseCases/Auth/SimpleAuth/SimpleAuthSchema.ts
@@ -1,8 +1,6 @@
 import * as yup from "yup";
 
 export const SimpleAuthSchema = yup.object({
-  email: yup.string()
-    .email().required(),
-  password: yup.string()
-    .required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
 });
