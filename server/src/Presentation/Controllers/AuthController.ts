4edd8ed62a91a9d1300d2093205c095dac756fc2
roney ruiz <roneyruizrojas@gmail.com>
2025-03-03 14:57:48 -0600

    feat(server): add forget password endpoint

--- server/src/Presentation/Controllers/AuthController.ts
+++ server/src/Presentation/Controllers/AuthController.ts
@@ -1,15 +1,19 @@
 import type { Request, Response } from "express";
 import type { SimpleAuthUseCase } from "@UseCases/Auth/SimpleAuth/SimpleAuthUseCase";
 import type { RegisterUserUseCase } from "@UseCases/Auth/RegisterUser/RegisterUserUseCase";
+import type { ForgetPasswordUseCase } from "@UseCases/Auth/ForgetPassword/ForgetPasswordUseCase";
 
 export class AuthController {
   constructor(
     private simpleAuthUseCase: SimpleAuthUseCase,
     private registerUserUseCase: RegisterUserUseCase,
+    private forgetPasswordUserCase: ForgetPasswordUseCase
   ) {
     this.simple = this.simple.bind(this);
+    this.forget = this.forget.bind(this);
     this.register = this.register.bind(this);
   };
+
 
   public async simple(req: Request, res: Response): Promise<void> {
     const auth = await this.simpleAuthUseCase.execute(req.body);
@@ -19,5 +23,10 @@ export class AuthController {
   public async register(req: Request, res: Response): Promise<void> {
     const auth = await this.registerUserUseCase.execute(req.body);
     res.status(200).json({ type: auth.type, token: auth.token });
+  };
+
+  public async forget(req: Request, res: Response): Promise<void> {
+    const forget = await this.forgetPasswordUserCase.execute(req.body);
+    res.status(200).json({ message: forget.message });
   };
 };
--- server/src/Presentation/Routers/AuthRouter.ts
+++ server/src/Presentation/Routers/AuthRouter.ts
@@ -6,17 +6,21 @@ import { UserRepository } from "@Repositories/UserRepository";
 
 import { SimpleAuthUseCase } from "@UseCases/Auth/SimpleAuth/SimpleAuthUseCase";
 import { RegisterUserUseCase } from "@UseCases/Auth/RegisterUser/RegisterUserUseCase";
+import { ForgetPasswordUseCase } from "@UseCases/Auth/ForgetPassword/ForgetPasswordUseCase";
 
 const repository = new UserRepository(Context);
 
 const simpleAuthUseCase = new SimpleAuthUseCase(repository);
 const registerUserUseCase = new RegisterUserUseCase(repository);
+const forgetPasswordUserCase = new ForgetPasswordUseCase(repository);
 
 const controller = new AuthController(
   simpleAuthUseCase, registerUserUseCase,
+  forgetPasswordUserCase,
 );
 
 export const AuthRouter = Router();
 
 AuthRouter.post("/login/simple", controller.simple);
 AuthRouter.post("/register", controller.register);
+AuthRouter.post("/forget", controller.forget);
