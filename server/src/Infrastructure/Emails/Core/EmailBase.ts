71ee8d8306471dcfa87d6e89d751500f4cb10bc6
roney ruiz <roneyruizrojas@gmail.com>
2025-03-07 21:03:51 -0600

    refactor(server): modify and adjust email template

---
+++ server/src/Infrastructure/Emails/Core/EmailBase.ts
@@ -0,0 +1,32 @@
+import type Mail from "nodemailer/lib/mailer";
+import { EmailTemplate } from "./EmailTemplate";
+import { EmailTransport } from "./EmailTransport";
+import { EmailConfig } from "@Configs/EmailConfig";
+
+export interface EmailProps {
+  to: string;
+};
+
+export class EmailBase {
+  protected template: string = "";
+  protected payload: any = {};
+
+  protected options: Mail.Options = {
+    from: EmailConfig.MAIL_ENTITY,
+  };
+
+  constructor(protected props: EmailProps) {
+    this.options.to = props.to;
+  };
+
+  protected async prepare(): Promise<void> {
+    const builder = new EmailTemplate(this.template);
+    await builder.load();
+    this.options.html = builder.obtain(this.payload);
+  };
+
+  public async send(): Promise<void> {
+    await this.prepare();
+    await EmailTransport.sendMail(this.options);
+  };
+};
