4ac49a3281933d13dd8c60f896e1787cb9972b5c
roney ruiz <roneyruizrojas@gmail.com>
2025-03-07 21:19:04 -0600

    chore(server): adjust email model typing

--- server/src/Infrastructure/Emails/ForgetPasswordEmail.ts
+++ server/src/Infrastructure/Emails/ForgetPasswordEmail.ts
@@ -1,16 +1,15 @@
 import { EmailBase, type EmailProps } from "./Core/EmailBase";
+import type { ForgetPasswordModel } from "./Models/ForgetPasswordModel";
 
-interface LocalEmailProps extends EmailProps {
-  token: string;
-};
+interface LocalEmailProps extends EmailProps, ForgetPasswordModel {};
 
-export class ForgetPasswordEmail extends EmailBase {
+export class ForgetPasswordEmail extends EmailBase<ForgetPasswordModel> {
   protected template: string = "ForgetPassword.mjml";
 
   constructor(props: LocalEmailProps) {
     super(props);
 
-    this.payload = { token: props.token };
+    this.model = { token: props.token };
     this.options.subject = "Forget Password";
   };
 };
