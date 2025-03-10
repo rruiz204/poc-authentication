872beee8dd2835c9a6982c837d5182c648ed4429
roney ruiz <roneyruizrojas@gmail.com>
2025-03-09 20:58:48 -0600

    feat(client): setup base files

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,10 @@ import "./style.css"
 import App from "./App.vue"
 import { createApp } from "vue"
 
+import { Router } from "@Routes/Router";
+
 const app = createApp(App);
+
+app.use(Router);
 
 app.mount("#app");
