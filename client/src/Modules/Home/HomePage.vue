d0f980b4a1f385edde43f21eb415eb567d13fde5
roney ruiz <roneyruizrojas@gmail.com>
2025-03-09 21:22:35 -0600

    chore(client): testing headless components

--- client/src/Modules/Home/HomePage.vue
+++ client/src/Modules/Home/HomePage.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
-
+import Button from 'primevue/button';
 </script>
 
 <template>
   <p class="text-3xl font-bold underline">home page works!</p>
+
+  <Button v-slot="slotProps" asChild>
+    <button v-bind="slotProps.a11yAttrs" class="font-bold"
+      @click="console.log('testing')">Testing</button>
+  </Button>
 </template>
