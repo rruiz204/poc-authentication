e28fb8a9bad7b6b6812a00a30f4068b40aaed4b6
roney ruiz <roneyruizrojas@gmail.com>
2025-03-09 21:59:25 -0600

    feat(client): build login and register page

--- client/src/Core/Routes/Publics.ts
+++ client/src/Core/Routes/Publics.ts
@@ -1,7 +1,11 @@
 import type { RouteRecordRaw } from "vue-router";
 
 import HomePage from "@Modules/Home/HomePage.vue";
+import LoginPage from "@Modules/Auth/Login/LoginPage.vue";
+import RegisterPage from "@Modules/Auth/Register/RegisterPage.vue";
 
 export const publics: RouteRecordRaw[] = [
   { path: "/", component: HomePage },
+  { path: "/login", component: LoginPage },
+  { path: "/register", component: RegisterPage },
 ];
---
+++ client/src/Modules/Auth/Register/RegisterPage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <p>register page works!</p>
+  </div>
+</template>
