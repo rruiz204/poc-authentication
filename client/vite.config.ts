b46e2631e49c42d354ac991839a14b376da04c78
roney ruiz <roneyruizrojas@gmail.com>
2025-03-09 20:59:04 -0600

    chore(client): config ts paths for vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: "@Routes", replacement: "/src/Core/Routes" },
+      { find: "@Modules", replacement: "/src/Modules" },
+    ],
+  },
 })
