66a36d76352076c75978db0c2283185311fc638b
roney ruiz <roneyruizrojas@gmail.com>
2025-03-11 07:26:07 -0600

    feat(server): write get my user use case unit test

---
+++ server/src/Tests/Unit/UseCases/User/GetMyUserUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it, vi } from "vitest";
+
+import { Context } from "@Database/Context";
+import { UnitOfWOrk } from "@Database/UnitOfWork";
+import { UserFactory } from "@Database/Factories/UserFactory";
+import { GetMyUserUseCase } from "@UseCases/User/GetMyUser/GetMyUserUseCase";
+
+import type { GetMyUserQuery } from "@UseCases/User/GetMyUser/GetMyUserQuery";
+
+describe(("get my user use case"), () => {
+  const uow = new UnitOfWOrk(Context);
+  const useCase = new GetMyUserUseCase(uow);
+
+  it("positive", async () => {
+    const user1 = await UserFactory.build({ id: 1, password: "12345678" });
+    const localQuery: GetMyUserQuery = {  userId: user1.id };
+    vi.spyOn(uow.user, "findById").mockResolvedValue(user1);
+
+    const user = await useCase.execute(localQuery);
+    expect(user.id).toEqual(user1.id);
+  });
+
+  it("negative", async () => {
+    const localQuery: GetMyUserQuery = { userId: 99 };
+    vi.spyOn(uow.user, "findById").mockResolvedValue(null);
+
+    const user = await useCase.execute(localQuery);
+    expect(user).toBeNull();
+  });
+});
