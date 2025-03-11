fedfa48d53d7c2a31ee07e88b50c553e78959deb
roney ruiz <roneyruizrojas@gmail.com>
2025-03-11 07:25:50 -0600

    feat(server): write list users use case unit test

--- server/src/Tests/Unit/UseCases/User/ListUsersUseCase.spec.ts
+++ server/src/Tests/Unit/UseCases/User/ListUsersUseCase.spec.ts
@@ -5,11 +5,25 @@ import { UnitOfWOrk } from "@Database/UnitOfWork";
 import { UserFactory } from "@Database/Factories/UserFactory";
 import { ListUsersUseCase } from "@UseCases/User/ListUsers/ListUsersUseCase";
 
+import type { ListUsersQuery } from "@UseCases/User/ListUsers/ListUsersQuery";
+
 describe(("list all users use case"), () => {
   const uow = new UnitOfWOrk(Context);
   const useCase = new ListUsersUseCase(uow);
+  const query: ListUsersQuery = { page: 1, limit: 10 };
 
   it("should return all users when products exist", async () => {
-    expect(2 + 2).toEqual(4);
+    const user1 = await UserFactory.build({ id: 1, password: "12345678" });
+    const user2 = await UserFactory.build({ id: 2, password: "12345678" });
+
+    vi.spyOn(uow.user, "list").mockResolvedValue([ user1, user2 ]);
+    const users = await useCase.execute(query);
+    expect(users.length).toEqual(2);
+  });
+
+  it("should return an empty list when no users exist", async () => {
+    vi.spyOn(uow.user, "list").mockResolvedValue([]);
+    const users = await useCase.execute(query);
+    expect(users.length).toEqual(0);
   });
 });
