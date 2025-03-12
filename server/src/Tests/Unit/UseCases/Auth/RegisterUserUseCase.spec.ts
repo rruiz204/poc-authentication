3ae32b960676731093e6580dd46082af765d0e9d
roney ruiz <roneyruizrojas@gmail.com>
2025-03-11 20:12:39 -0600

    feat(server): write register user use case unit test

---
+++ server/src/Tests/Unit/UseCases/Auth/RegisterUserUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it, vi } from "vitest";
+
+import { Context } from "@Database/Context";
+import { UnitOfWOrk } from "@Database/UnitOfWork";
+import { JwtService } from "@Services/JwtService/Service";
+import { UserFactory } from "@Database/Factories/UserFactory";
+
+import { RegisterUserUseCase } from "@UseCases/Auth/RegisterUser/RegisterUserUseCase";
+import type { RegisterUserCommand } from "@UseCases/Auth/RegisterUser/RegisterUserCommand";
+
+describe(("update user use case"), () => {
+  const uow = new UnitOfWOrk(Context);
+  const useCase = new RegisterUserUseCase(uow);
+
+  it("positive", async () => {
+    const user1 = await UserFactory.build({ id: 1, password: "12345678" });
+    const command: RegisterUserCommand = { ...user1 };
+
+    vi.spyOn(JwtService, "sign").mockResolvedValue("mocked token");
+    vi.spyOn(uow.user, "findByEmail").mockResolvedValue(null);
+    vi.spyOn(uow.user, "create").mockResolvedValue(user1);
+
+    const response = await useCase.execute(command);
+    expect(response.token).toEqual("mocked token");
+  });
+
+  it("negative", async () => {
+    const user1 = await UserFactory.build({ id: 1, password: "12345678" });
+    const command: RegisterUserCommand = { ...user1 };
+
+    vi.spyOn(uow.user, "findByEmail").mockResolvedValue(user1);
+    
+    await expect(useCase.execute(command))
+      .rejects.toThrowError("User already exists");
+  });
+});
