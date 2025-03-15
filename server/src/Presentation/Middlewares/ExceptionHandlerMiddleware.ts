7fac62f5a2875e33d00d018c28d8a30441de11fb
roney ruiz <roneyruizrojas@gmail.com>
2025-03-15 11:48:02 -0600

    chore(server): rename exception handler middleware

---
+++ server/src/Presentation/Middlewares/ExceptionHandlerMiddleware.ts
@@ -0,0 +1,7 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const ExceptionHandlerMiddleware = async (
+  err: Error, req: Request, res: Response, next: NextFunction
+) => {
+  res.status(500).json({ message: err.message });
+};
--- server/src/Presentation/Middlewares/ExceptionMiddleware.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-
-export const ExceptionMiddleware = async (err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-};
