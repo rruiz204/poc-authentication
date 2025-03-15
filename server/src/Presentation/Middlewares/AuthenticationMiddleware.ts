b3f55ac48e8751a9684854f5143ba1fedfd4c00f
roney ruiz <roneyruizrojas@gmail.com>
2025-03-15 16:58:17 -0600

    feat(server): build authentication middleware

--- server/src/Presentation/Middlewares/AuthenticationMiddleware.ts
+++ server/src/Presentation/Middlewares/AuthenticationMiddleware.ts
@@ -3,8 +3,9 @@ import type { Request } from "express";
 import { JwtService } from "@Services/JwtService";
 import type { JwtPayload } from "@Services/JwtService";
 
-export const AuthMiddleware = async (req: Request): Promise<JwtPayload> => {
-  let token = req.headers.authorization || "";
+export const AuthenticationMiddleware = async (req: Request): Promise<JwtPayload | null> => {
+  if ((req.body.query as string).includes("IntrospectionQuery")) return null;
+  const token = req.headers.authorization || "";
 
   if (!token || !token.startsWith("Bearer")) {
     throw new GraphQLError("User is not authenticated");
