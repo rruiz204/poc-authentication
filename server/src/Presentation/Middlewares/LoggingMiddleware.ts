b31cbe11b5f769b7625ae6e3807f1367ef7429b6
roney ruiz <roneyruizrojas@gmail.com>
2025-03-16 15:33:06 -0600

    format(server): improve logging middleware structure

--- server/src/Presentation/Middlewares/LoggingMiddleware.ts
+++ server/src/Presentation/Middlewares/LoggingMiddleware.ts
@@ -6,12 +6,7 @@ const EXCLUDED_PATHS: string[] = ["/graphql"];
 const logger = new LoggerService()
   .setConsole().setFilePath("logs/rest.log").getLogger();
 
-export const LoggerMiddleware = async (
-  req: Request, res: Response, next: NextFunction
-) => {
-  if (!EXCLUDED_PATHS.includes(req.path)) {
-    logger.info(`${req.method} ${req.url}`);
-  };
-
+export const LoggerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  if (!EXCLUDED_PATHS.includes(req.path)) logger.info(`${req.method} ${req.url}`);
   next();
 };
