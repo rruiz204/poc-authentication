aed977e1159a10553a0378364fb0c7848fcf67fe
roney ruiz <roneyruizrojas@gmail.com>
2025-03-16 23:58:52 -0600

    feat(client): build field component

---
+++ client/src/Components/Common/Atoms/Field.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { Message } from "primevue";
+
+defineProps<{
+  invalid: boolean;
+  message: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <slot></slot>
+    <Message v-if="invalid" severity="error" size="small" variant="simple">
+      {{ message }}
+    </Message>
+  </div>
+</template>
