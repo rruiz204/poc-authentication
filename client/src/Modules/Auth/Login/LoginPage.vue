ed64ef235a416c9ed0c9472826958e4d09e19893
roney ruiz <roneyruizrojas@gmail.com>
2025-03-17 23:32:34 -0600

    feat(client): working on add information for login page

--- client/src/Modules/Auth/Login/LoginPage.vue
+++ client/src/Modules/Auth/Login/LoginPage.vue
@@ -3,7 +3,11 @@ import LoginForm from './LoginForm.vue';
 </script>
 
 <template>
-  <div>
+  <div class="w-screen min-h-screen flex flex-col items-center justify-center gap-4">
+    <div class="p-2">
+      <p class="text-2xl font-semibold text-center">Please login to continue</p>
+    </div>
+
     <LoginForm />
   </div>
 </template>
