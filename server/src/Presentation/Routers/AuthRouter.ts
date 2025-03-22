d012123cd197e007fce41b3446bc78bc6cc5faa7
roney ruiz <roneyruizrojas@gmail.com>
2025-03-21 22:26:51 -0600

    feat(server): refresh token endpoint ready

--- server/src/Presentation/Routers/AuthRouter.ts
+++ server/src/Presentation/Routers/AuthRouter.ts
@@ -5,12 +5,14 @@ import { AuthController } from "@Controllers/AuthController";
 
 import { SimpleAuthUseCase } from "@UseCases/Auth/SimpleAuth/SimpleAuthUseCase";
 import { RegisterUserUseCase } from "@UseCases/Auth/RegisterUser/RegisterUserUseCase";
+import { RefreshTokenUseCase } from "@UseCases/Auth/RefreshToken/RefreshTokenUseCase";
 import { ResetPasswordUseCase } from "@UseCases/Auth/ResetPassword/ResetPasswordUseCase";
 import { ForgetPasswordUseCase } from "@UseCases/Auth/ForgetPassword/ForgetPasswordUseCase";
 
 const uow = new UnitOfWOrk(Context);
 
 const simpleAuthUseCase = new SimpleAuthUseCase(uow);
+const refreshTokenUseCase = new RefreshTokenUseCase();
 const registerUserUseCase = new RegisterUserUseCase(uow);
 const resetPasswordUseCase = new ResetPasswordUseCase(uow);
 const forgetPasswordUserCase = new ForgetPasswordUseCase(uow);
@@ -18,6 +20,7 @@ const forgetPasswordUserCase = new ForgetPasswordUseCase(uow);
 const controller = new AuthController(
   simpleAuthUseCase,
   registerUserUseCase,
+  refreshTokenUseCase,
   resetPasswordUseCase,
   forgetPasswordUserCase,
 );
@@ -26,5 +29,6 @@ export const AuthRouter = Router();
 
 AuthRouter.post("/login/simple", controller.simple);
 AuthRouter.post("/register", controller.register);
+AuthRouter.post("/refresh", controller.refresh);
 AuthRouter.post("/forget", controller.forget);
 AuthRouter.post("/reset", controller.reset);
