826b4f55163e23c974c4f1b6aba59f8d6a4fcd81
roney ruiz <roneyruizrojas@gmail.com>
2025-04-29 13:33:58 -0600

    chore(server): remove old files

--- server/src/Infrastructure/Configs/JwtConfig.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-const schema = yup.object({
-  JWT_SECRET: yup.string().required(),
-  JWT_ACCESS_EXPIRATION: yup.string().required(),
-  JWT_REFRESH_EXPIRATION: yup.string().required(),
-});
-
-export const JwtConfig = schema.validateSync(process.env);
