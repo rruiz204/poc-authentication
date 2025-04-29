5652635360339da700eb881b95badefae0136834
roney ruiz <roneyruizrojas@gmail.com>
2025-04-28 20:57:59 -0600

    chore(server): remove old migrations

--- server/prisma/migrations/20250224021449_create_seed_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Seed" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Seed_name_key" ON "Seed"("name");
--- server/prisma/migrations/20250304000709_create_reset_token_table/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "ResetToken" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "token" TEXT NOT NULL,
-    "user_id" INTEGER NOT NULL
-);
