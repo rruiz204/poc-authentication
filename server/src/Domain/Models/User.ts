f28a569c35f46a4539ada039f5888271ba0cbea6
roney ruiz <roneyruizrojas@gmail.com>
2025-04-29 07:26:09 -0600

    feat(server): create user model and adjust context

---
+++ server/src/Domain/Models/User.ts
@@ -0,0 +1,14 @@
+import { Prisma } from "generated/prisma";
+import type { User as Model } from "generated/prisma";
+
+export namespace User {
+  export type Entity = Model;
+  export type Records = Model[];
+  export type Nullable = Model | null;
+
+  export type CreateParams = Prisma.UserCreateInput;
+  export type UpdateParams = Prisma.UserUpdateInput;
+
+  export type WhereParams = Prisma.UserWhereInput;
+  export type OrderParams = Prisma.UserOrderByWithRelationInput;
+};
