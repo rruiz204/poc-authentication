1a999cd95249aa81be2a8743336bd5cfc83b9219
roney ruiz <roneyruizrojas@gmail.com>
2025-05-01 23:40:58 -0600

    feat(server): create simple register use case

---
+++ server/src/Application/UseCases/Auth/SimpleRegister/SimpleRegisterSchema.ts
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+export const SimpleRegisterSchema = yup.object({
+  active: yup.boolean(),
+  name: yup.string().required().min(3).max(30),
+  email: yup.string().required().email().max(50),
+  password: yup.string().required().min(8).max(25),
+});
