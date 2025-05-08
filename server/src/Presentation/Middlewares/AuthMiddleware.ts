2568783ce9f9d2e386bb1ebecabdf5c379110491
roney ruiz <roneyruizrojas@gmail.com>
2025-05-07 21:49:55 -0600

    feat(server): create auth middleware

---
+++ server/src/Presentation/Middlewares/AuthMiddleware.ts
@@ -0,0 +1,15 @@
+import { JwtService } from "@Services/Tokens/JwtService";
+import { HttpException } from "@Exceptions/HttpException";
+import type { Request, Response, NextFunction } from "express";
+
+export const AuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) throw new HttpException.Unauthorized("Invalid token");
+
+  try {
+    req.user = await JwtService.verify(token);
+    next();
+  } catch {
+    throw new HttpException.Unauthorized("Bad token");
+  };
+};
